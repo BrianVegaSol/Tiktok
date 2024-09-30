2e71f30ca5f1c10297d11073514957678722f364
BrianVegaSol <brivegasol@gmail.com>
2024-09-30 16:25:51 -0700

    "Fixed"
    Code runs but always goes into Invalid input statement from while loop

--- src/main/java/com/pluarlsight/CalculatorASMD.java
+++ src/main/java/com/pluarlsight/CalculatorASMD.java
@@ -3,7 +3,8 @@ package com.pluarlsight;
 import java.util.Scanner;
 
 public class CalculatorASMD {
-    /*public static int add(int number1, int number2) {
+    //Challenging myself with this to use a while loop
+    public static int add(int number1, int number2) {
         return number1 + number2;
     }
 
@@ -17,9 +18,10 @@ public class CalculatorASMD {
 
     public static int multiply(int num1, int num2) {
         return num1 * num2;
-    }*/
+    }
 
-    public static float add(float number1, float number2) {
+    //How the code should be written
+   /* public static float add(float number1, float number2) {
         return number1 + number2;
     }
 
@@ -33,7 +35,7 @@ public class CalculatorASMD {
 
     public static float multiply(float num1, float num2) {
         return num1 * num2;
-    }
+    }*/
 
     public static void main(String[] args) {
         //If the while works, a l w a y s initialize vars early so there arent scope issues
@@ -51,22 +53,27 @@ public class CalculatorASMD {
                 "\n(D)ivision");
         char operation = scan.next().charAt(0);
         char operationCheck = Character.toUpperCase(operation);
+        System.out.println(operationCheck);
 
         //TRY DECLARING NUMS OUTSIDE OF THE SCOPE OF THE WHILE LOOP
         //SO THAT THE SWITCH DOESNT FLIP OUT MAN
 
-
-        float num1 = 0f;
-        float num2 = 0f;
-
-
+        int num1 = 0;
+        int num2 = 0;
+        float fnum1 = 0f;
+        float fnum2 = 0f;
 
         //All this is unnecessary if in the method data types are all float/doubles
-       /* while(operationCheck != 'A' || operationCheck != 'S' || operationCheck != 'M' || operationCheck != 'D') {
+        //Code is forced into Invalid input statement despite being correct
+        //Why is the while statement always set to true?
+       while(operationCheck != 'A' || operationCheck != 'S' || operationCheck != 'M' || operationCheck != 'D') {
             System.out.println("Invalid input");
             operation = scan.next().charAt(0);
             operationCheck = Character.toUpperCase(operation);
-        }*/
+            if(operationCheck == 'A' || operationCheck == 'S' || operationCheck == 'M' || operationCheck == 'D'){
+                break;
+           }
+        }
             if (operationCheck == 'A' || operationCheck == 'S' || operationCheck == 'M') {
                 System.out.println("Enter your first number: ");
                 num1 = scan.nextInt();
@@ -74,20 +81,10 @@ public class CalculatorASMD {
                 num2 = scan.nextInt();
             } else if (operationCheck == 'D') {
                 System.out.println("Enter your first number: ");
-                num1 = scan.nextFloat();
+                fnum1 = scan.nextFloat();
                 System.out.println("Enter your second number: ");
-                num2 = scan.nextFloat();
+                fnum2 = scan.nextFloat();
             }
-
-        //
-
-
-
-        //Hopefully initializing nums before while loop works so I can delete this
-
-        //
-
-
             switch (operationCheck) {
                 case 'A':
                     System.out.println(num1 + " + " + num2 + " = " + add(num1, num2));
@@ -99,12 +96,9 @@ public class CalculatorASMD {
                     System.out.println(num1 + " * " + num2 + " = " + multiply(num1, num2));
                     break;
                 case 'D':
-                    System.out.println(num1 + " / " + num2 + " = " + division(num1, num2));
+                    System.out.println(fnum1 + " / " + fnum2 + " = " + division(num1, num2));
                     break;
 
             }
-
-
         }
-
     }
